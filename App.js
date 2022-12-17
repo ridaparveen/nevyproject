7e9082c09bda143136065f83e2cfb835098e0e65
ridaparveen <118651918+ridaparveen@users.noreply.github.com>
2022-12-17 20:02:43 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './component/Basics/Navbar';
+const App = () => {
+  return (
+    <>
+       <Navbar/>
+      
+      </>
+    
+  )
+  }
+
+export default App
+
---
+++ menuApi.js
@@ -0,0 +1,90 @@
+const menu = [{
+    id:"Favipriver 400mg (febiflew) tablet",
+    Images:"Images/febiflu_jpeg.jpg",
+    name:"RS 1775/stripe",
+    category:"Glenmark Pharmectital Limited",
+    price:25,
+    description:"(Parvat patiya surat gujrat)"
+},
+{
+    id:"Favipriver 400mg (febiflew) tablet",
+    Images:"Images/febiflu_jpeg.jpg",
+    name:"RS 1775/stripe",
+    category:"Glenmark Pharmectital Limited",
+    price:25,
+    description:"(Parvat patiya surat gujrat)"
+},
+{
+    id:"Favipriver 400mg (febiflew) tablet",
+    Images:"Images/febiflu_jpeg.jpg",
+    name:"RS 1775/stripe",
+    category:"Glenmark Pharmectital Limited",
+    price:25,
+    description:"(Parvat patiya surat gujrat)"
+},
+{
+    id:"Favipriver 400mg (febiflew) tablet",
+    Images:"Images/febiflu_jpeg.jpg",
+    name:"RS 1775/stripe",
+    category:"Glenmark Pharmectital Limited",
+    price:25,
+    description:"(Parvat patiya surat gujrat)"
+},
+{
+    id:"Favipriver 400mg (febiflew) tablet",
+    Images:"Images/febiflu_jpeg.jpg",
+    name:"RS 1775/stripe",
+    category:"Glenmark Pharmectital Limited",
+    price:25,
+    description:"(Parvat patiya surat gujrat)"
+},
+{
+    id:"Favipriver 400mg (febiflew) tablet",
+    Images:"Images/febiflu_jpeg.jpg",
+    name:"RS 1775/stripe",
+    category:"Glenmark Pharmectital Limited",
+    price:25,
+    description:"(Parvat patiya surat gujrat)"
+},
+{
+    id:"Favipriver 400mg (febiflew) tablet",
+    Images:"Images/febiflu_jpeg.jpg",
+    name:"RS 1775/stripe",
+    category:"Glenmark Pharmectital Limited",
+    price:25,
+    description:"(Parvat patiya surat gujrat)"
+},
+{
+    id:"Favipriver 400mg (febiflew) tablet",
+    Images:"Images/febiflu_jpeg.jpg",
+    name:"RS 1775/stripe",
+    category:"Glenmark Pharmectital Limited",
+    price:25,
+    description:"(Parvat patiya surat gujrat)"
+},
+{
+    id:"Favipriver 400mg (febiflew) tablet",
+    Images:"Images/febiflu_jpeg.jpg",
+    name:"RS 1775/stripe",
+    category:"Glenmark Pharmectital Limited",
+    price:25,
+    description:"(Parvat patiya surat gujrat)"
+},
+{
+    id:"Favipriver 400mg (febiflew) tablet",
+    Images:"Images/febiflu_jpeg.jpg",
+    name:"RS 1775/stripe",
+    category:"Glenmark Pharmectital Limited",
+    price:25,
+    description:"(Parvat patiya surat gujrat)"
+},
+{
+    id:"Favipriver 400mg (febiflew) tablet",
+    Image:"Images/febiflu_jpeg.jpg",
+    name:"RS 1775/stripe",
+    category:"Glenmark Pharmectital Limited",
+    price:25,
+    description:"(Parvat patiya surat gujrat)"
+},
+];
+export default menu;
